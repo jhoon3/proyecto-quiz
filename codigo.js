869d4d356e0fb602b9d998bc0072910fe5b37d09
jhoncristhian3 <jhohunter33@gmail.com>
2025-12-19 15:59:57 -0400

    Add files via upload

---
+++ codigo.js
@@ -0,0 +1,98 @@
+export const preguntasyrespuestas = {
+    "Desarrollo Web":[
+        {
+            "pregunta": "¿Qué significa HTML?",
+            "respuestas":[
+                "HyperText Makeup Language",
+                "HyperText Markup Language ",
+                "HighText Machine Language",
+                "HyperTransfer Markup Language"
+            ],
+            "respuesta correcta": "HyperText Markup Language "
+        },
+        {
+            "pregunta": "¿Qué es un navegador web?",
+            "respuestas":[
+                "Un programa para dibujar",
+                "Una (aplicación o programa)  para buscar y ver páginas en internet ",
+                "Un videojuego",
+                "Un tipo de archivo"
+            ],
+            "respuesta correcta": "Una (aplicación o programa)  para buscar y ver páginas en internet "
+        },
+        {
+            "pregunta": "¿Cuál de los siguientes es un editor de código popular para desarrollo web?",
+            "respuestas":[
+                "Microsoft Word",
+                "Adobe Photoshop",
+                "Visual Studio Code ",
+                " Excel"
+            ],
+            "respuesta correcta": "Visual Studio Code"
+        },    
+    ],
+    "informatica":[
+        {
+            "pregunta": "¿Qué es la informática?",
+            "respuestas":[
+                "El estudio de los animales",
+                "La ciencia que estudia el uso y manejo de la información mediante computadoras ",
+                "Un programa de dibujo",
+                "Un tipo de red social"
+            ],
+            "respuesta correcta": "La ciencia que estudia el uso y manejo de la información mediante computadoras "
+        },
+        {
+            "pregunta": "¿Qué es un software?",
+            "respuestas":[
+                "Las partes que se pueden tocar de una computadora",
+                "Los programas y aplicaciones que usamos en la computadora ",
+                "Un tipo de cable",
+                "Un componente de red"
+            ],
+            "respuesta correcta": "Los programas y aplicaciones que usamos en la computadora "
+        },
+        {
+            "pregunta": "Es conocido como el “padre de la computación”",
+            "respuestas":[
+                "Donald Knuth",
+                "Charles Babbage ",
+                "Linus Torvalds",
+                "Berners Lee"
+            ],
+            "respuesta correcta": "Charles Babbage"
+        },
+    ],
+    "programacion":[
+        {
+            "pregunta": "¿Qué es un lenguaje de programación?",
+            "respuestas":[
+                "Un lenguaje usado solo por expertos",
+                "Un conjunto de instrucciones que sirven para crear programas ",
+                "Un traductor de idiomas",
+                "Una herramienta para editar fotos"
+            ],
+            "respuesta correcta": "Un conjunto de instrucciones que sirven para crear programas"
+        },
+        {
+            "pregunta": "¿Qué es un algoritmo?",
+            "respuestas":[
+                "Un error en el código",
+                "Un conjunto de pasos para resolver un problema ",
+                "Un lenguaje de programación",
+                "Una variable"
+            ],
+            "respuesta correcta": "Un conjunto de pasos para resolver un problema "
+        },
+        {
+            "pregunta": "¿¿Cuál de los siguientes operadores se utiliza para comparar igualdad en Java??",
+            "respuestas":[
+                "=",
+                "==",
+                "!=",
+                "==="
+            ],
+            "respuesta correcta": "=="
+        },
+    ]
+}
